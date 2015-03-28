36f6ac68254be65e1c49ea3fd11c6d50510beedf
Ernest Walzel <ernest.walzel@ed.ac.uk>
2015-03-28 16:49:00 +0000

    shout command

--- src/main/java/eronisko/minecraft/plugin/firstplugin/FirstPlugin.java
+++ src/main/java/eronisko/minecraft/plugin/firstplugin/FirstPlugin.java
@@ -1,6 +1,9 @@
 package eronisko.minecraft.plugin.firstplugin;
 
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,5 +15,31 @@ public class FirstPlugin extends JavaPlugin {
         for (Player player: Bukkit.getServer().getOnlinePlayers()) {
             player.sendMessage("Hello, " + player.getDisplayName());
         }
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("shout")) {
+            if (!(sender instanceof Player)) {
+                // This command can only be run by a player.
+                return false;
+            }
+
+            if (args.length < 1) {
+                sender.sendMessage("What do you want to shout out?");
+                return false;
+            }
+
+            Player player = (Player) sender;
+
+            // Broadcast a messages to all players on the server
+            for (Player receiver: Bukkit.getServer().getOnlinePlayers()) {
+                String message = StringUtils.join(args, " ");
+                receiver.sendMessage(player.getDisplayName() + ": " + message);
+            }
+            return true;
+
+        }
+        return false;
     }
 }
