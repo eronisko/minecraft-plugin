83b6e68a4317c12d1c4b5cc3895a228147deb63d
Ernest Walzel <ernest.walzel@ed.ac.uk>
2015-03-28 17:37:00 +0000

    Lightning if you hold a stick

--- src/main/java/eronisko/minecraft/plugin/firstplugin/FirstPlugin.java
+++ src/main/java/eronisko/minecraft/plugin/firstplugin/FirstPlugin.java
@@ -2,12 +2,18 @@ package eronisko.minecraft.plugin.firstplugin;
 
 import org.apache.commons.lang.StringUtils;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class FirstPlugin extends JavaPlugin {
+import java.util.Set;
+
+public class FirstPlugin extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
@@ -15,6 +21,9 @@ public class FirstPlugin extends JavaPlugin {
         for (Player player: Bukkit.getServer().getOnlinePlayers()) {
             player.sendMessage("Hello, " + player.getDisplayName());
         }
+
+        //Register events
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
@@ -38,8 +47,17 @@ public class FirstPlugin extends JavaPlugin {
                 receiver.sendMessage(player.getDisplayName() + ": " + message);
             }
             return true;
-
         }
         return false;
+    }
+
+    @EventHandler
+    public void onPlayerInteractBlock(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+
+        if (player.getItemInHand().getType() == Material.STICK) {
+            // Creates a bolt of lightning at a given location. In this case, that location is where the player is looking.
+            player.getWorld().strikeLightning(player.getTargetBlock((Set<Material>) null, 10).getLocation());
+        }
     }
 }
