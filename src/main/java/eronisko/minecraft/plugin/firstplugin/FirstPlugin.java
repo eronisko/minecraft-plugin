f01bbc3a7246cc5e1e43759d61ec85478940e8ad
Ernest Walzel <ernest.walzel@ed.ac.uk>
2015-03-28 16:25:28 +0000

    Broadcast a message to players onEnable

--- src/main/java/eronisko/minecraft/plugin/firstplugin/FirstPlugin.java
+++ src/main/java/eronisko/minecraft/plugin/firstplugin/FirstPlugin.java
@@ -1,7 +1,16 @@
 package eronisko.minecraft.plugin.firstplugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FirstPlugin extends JavaPlugin {
 
+    @Override
+    public void onEnable() {
+        // Broadcast a messages to all players on the server
+        for (Player player: Bukkit.getServer().getOnlinePlayers()) {
+            player.sendMessage("Hello, " + player.getDisplayName());
+        }
+    }
 }
